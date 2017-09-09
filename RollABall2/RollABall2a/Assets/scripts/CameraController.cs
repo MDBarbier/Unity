7963cec814bfd39b14d6a46f1b8c3d4ba7606ffd
SirBarbier <matthew-7.7@hotmail.com>
2017-09-09 14:20:09 +0100

    v2

---
+++ RollABall2/RollABall2a/Assets/scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        offset = this.transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    //Guaranteed to run after each object for the frame (better for follow camera,last known states, player information)
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ RollABall2/RollABall2a/Assets/scripts/PlayerController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    //Public Variables (can be accessed from editor)
+    public Rigidbody rb;
+    public float speed;
+    private int scoreCount;
+    public Text scoreText;
+    public Text winText;
+    public Stopwatch stopwatch;
+
+	// Use this for initialization
+	void Start ()
+    {
+
+        //Try and find the rigidbody
+        rb = GetComponent<Rigidbody>();
+
+        //Set score initial
+        scoreCount = 0;
+
+        //Set score text
+        SetScoreText();
+
+        //Set victory message empty
+        winText.text = "";
+
+        stopwatch = new Stopwatch();
+        stopwatch.Start();
+    }
+
+    private void SetScoreText()
+    {
+        scoreText.text = "Score: " + scoreCount.ToString();
+
+        if (scoreCount >= 16)
+        {
+            string timeTaken = stopwatch.Elapsed.Seconds.ToString() + "." + stopwatch.Elapsed.Milliseconds;
+            winText.text = "Congratulations, you have collected all the shinies! You took " + timeTaken + " seconds!";
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    //Called whenever Physics is updated
+    private void FixedUpdate()
+    {
+        //Gets the movement from the KB
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        //Add Force to RigidBody
+        rb.AddForce(new Vector3(moveHorizontal, 0.0f, moveVertical) * speed);
+
+        
+    }
+
+    //Called when the player object collides with another object
+    private void OnTriggerEnter(Collider other)
+    {
+        //Destroy(other.gameObject); //This is how you could remove a GO completely and all children
+
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+            scoreCount++;
+            SetScoreText();
+        }
+    }
+}
