053ff8bcbdf5f3a3c76e631fcaa4ac801816b87b
matt barbier <matthewdavidbarbier@gmail.com>
2019-06-17 21:14:34 +0100

    camera lerping and player material

---
+++ 2dPlatformerFirstAttempt/Assets/Scripts/CameraController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject target; //The target object for the samera
+    public float leadTargetAmount; //the amount the camera should lead the target by
+    private Vector3 cameraTarget; //target for the camera to follow
+    public float cameraMoveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        cameraTarget = new Vector3(target.transform.position.x, transform.position.y, transform.position.z); //we use the x axis position of the target, but keep the y and z axis the same
+
+        if (target.transform.localScale.x > 0f)
+        {
+            //facing right
+            cameraTarget = new Vector3(cameraTarget.x + leadTargetAmount, cameraTarget.y, cameraTarget.z);           
+
+        }
+        else
+        {
+            //facing left
+            cameraTarget = new Vector3(cameraTarget.x - leadTargetAmount, cameraTarget.y, cameraTarget.z);            
+
+        }
+
+        //We use Lerp to move the camera smoothly
+        //Time.deltaTime is a way to account for different speed cpus
+        transform.position = Vector3.Lerp(transform.position, cameraTarget, cameraMoveSpeed * Time.deltaTime);
+        
+    }
+}
