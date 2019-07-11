ea87224cdf90875a9b0e3b7a9be3fe9a717c04f0
matt barbier <matthewdavidbarbier@gmail.com>
2019-07-11 11:52:03 +0100

    enemy stomping, death particle effect, patrolling enemies

---
+++ 2dPlatformerFirstAttempt/Assets/Scripts/OozeController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OozeController : MonoBehaviour
+{
+    public Transform leftPatrolLimit;
+    public Transform rightPatrolLimit;
+    public float moveSpeed;
+    private Rigidbody2D myRigidBody;
+    public Direction moveDirection;
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        if (moveDirection == Direction.Right && transform.position.x >= rightPatrolLimit.position.x)
+        {
+            moveDirection = Direction.Left;
+        }
+        else if (moveDirection == Direction.Left && transform.position.x <= leftPatrolLimit.position.x)
+        {
+            moveDirection = Direction.Right;
+        }
+        
+        if (moveDirection == Direction.Right)
+        {
+            myRigidBody.velocity = new Vector3(moveSpeed, myRigidBody.velocity.y, 0f);
+        }
+        else
+        {
+            myRigidBody.velocity = new Vector3(-moveSpeed, myRigidBody.velocity.y, 0f);
+        }
+    }
+
+    public enum Direction
+    {
+        Left,Right
+    }
+}
---
+++ 2dPlatformerFirstAttempt/Assets/Scripts/StompEnemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StompEnemy : MonoBehaviour
+{
+
+    public GameObject deathsplosionEffect;
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Enemy")
+        {
+            //fire particle effect
+            Instantiate(deathsplosionEffect, other.gameObject.transform.position, new Quaternion(other.gameObject.transform.rotation.x + 90f, other.gameObject.transform.rotation.y, 
+                other.gameObject.transform.rotation.z, other.gameObject.transform.rotation.w));
+
+            Destroy(other.gameObject);
+        }
+    }
+}
