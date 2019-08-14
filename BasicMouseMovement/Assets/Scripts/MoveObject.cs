d9473e921f4a12673ae2ec79660a77fbbe61f22b
matt barbier <matthewdavidbarbier@gmail.com>
2019-08-14 21:17:31 +0100

    added project which demonstrates using raycasting to detect mouse clicks in a 3d game

---
+++ BasicMouseMovement/Assets/Scripts/MoveObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObject : MonoBehaviour
+{
+    public GameObject objectToMove;
+    public float moveForce;
+    public Rigidbody rig;
+
+    public void MoveTheObject()
+    {
+        Vector3 movement = new Vector3(moveForce, moveForce/2);
+        //rig.AddForce(rig.transform.up * moveForce, ForceMode.Impulse);
+        rig.AddForce(movement, ForceMode.Impulse);
+    }
+}
---
+++ BasicMouseMovement/Assets/Scripts/RaycastForObjectName.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastForObjectName : MonoBehaviour
+{
+    private Vector3 targetPosition;
+    private Camera mainCamera;
+    private Rigidbody rig;
+    private MoveObject moveObjectScript;
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+        mainCamera = Camera.main;
+        moveObjectScript = FindObjectOfType<MoveObject>();
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        RaycastHit hit;
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(ray, out hit, 100f))
+            {
+                if (hit.transform)
+                {
+                    Debug.Log($"Ray hit: {hit.transform.gameObject}");
+
+                    if (rig = hit.transform.GetComponent<Rigidbody>())
+                    {                        
+                        moveObjectScript.MoveTheObject();
+                    }
+                }
+            }
+        }
+    }
+}
