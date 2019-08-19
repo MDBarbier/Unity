a54d91f2babfe8489d0ad0a5ca4af43608a81687
matt barbier <matthewdavidbarbier@gmail.com>
2019-08-19 20:08:49 +0100

    finished camera rotation and height adjustment

--- camera_move_demo/Assets/Scripts/CameraController.cs
+++ camera_move_demo/Assets/Scripts/CameraController.cs
@@ -1,21 +1,85 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
     public float cameraMoveSpeed;
+    public float cameraMousePanSpeed;
+    public float panDetect;
+    public float rotationSpeed;
+    public float rotationAmount;
+    public float minCameraHeight;
+    public float maxCameraHeight;
+
+    private Quaternion rotation;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rotation = Camera.main.transform.rotation;
     }
 
     // Update is called once per frame
     void Update()
     {
-        /* Lateral movement */
+        MoveCamera();
+        RotateCamera();
+    }
+
+    private void RotateCamera()
+    {
+        Vector3 origin = Camera.main.transform.eulerAngles;
+        Vector3 destination = origin;
+
+        if (Input.GetMouseButton(2))
+        {
+            destination.x -= Input.GetAxis("Mouse Y") * (rotationAmount);
+            destination.y += Input.GetAxis("Mouse X") * (rotationAmount);
+        }
+
+        if (destination != origin)
+        {
+            Camera.main.transform.eulerAngles = Vector3.MoveTowards(origin, destination, Time.deltaTime * rotationSpeed);
+        }
+    }
+
+    /// <summary>
+    /// Handles panning movement both by the keyboard and the mouse being near the edge of the screen
+    /// </summary>
+    private void MoveCamera()
+    {
+        ScreenEdgePan();
+        LateralKeyboardMovement();
+        VerticalKeyboardMovement();
+        MouseScrollWheelMovement();
+    }
+
+    private void MouseScrollWheelMovement()
+    {
+        float moveY = Camera.main.transform.position.y;
+        float scrollwheel = Input.GetAxis("Mouse ScrollWheel");
+        moveY -= scrollwheel * (cameraMousePanSpeed * 20);
+        moveY = Mathf.Clamp(moveY, minCameraHeight, maxCameraHeight);
+        Vector3 newPos = new Vector3(Camera.main.transform.position.x, moveY, Camera.main.transform.position.z);
+        Camera.main.transform.position = newPos;
+    }
+
+    private void VerticalKeyboardMovement()
+    {
+        if ((Input.GetKey(KeyCode.PageUp)) && transform.position.y < maxCameraHeight)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + cameraMoveSpeed, transform.position.z);
+        }
+
+        if (Input.GetKey(KeyCode.PageDown) && transform.position.y > minCameraHeight)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y - cameraMoveSpeed, transform.position.z);
+        }
+    }
+
+    private void LateralKeyboardMovement()
+    {
         if (Input.GetKey(KeyCode.RightArrow))
         {
             transform.position = new Vector3(transform.position.x + cameraMoveSpeed, transform.position.y, transform.position.z);
@@ -32,35 +96,36 @@ public class CameraController : MonoBehaviour
         {
             transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + cameraMoveSpeed);
         }
-        /*end lateral movement*/
+    }
 
+    private void ScreenEdgePan()
+    {
+        float moveX = Camera.main.transform.position.x;
+        float moveZ = Camera.main.transform.position.z;
+        float mouseX = Input.mousePosition.x;
+        float mouseY = Input.mousePosition.y;
+        float cameraHeight = transform.position.y;
 
-        /*vertical movement */
-        if (Input.GetKey(KeyCode.PageUp))
+        if (mouseX > 0 && mouseX < panDetect) //mouse pan left
         {
-            transform.position = new Vector3(transform.position.x, transform.position.y + cameraMoveSpeed, transform.position.z);
+            moveX -= cameraMoveSpeed;
+        }
+        else if (mouseX < Screen.width && mouseX > Screen.width - panDetect) //mouse pan right
+        {
+            moveX += cameraMoveSpeed;
         }
 
-        if (Input.GetKey(KeyCode.PageDown))
+        if (mouseY < Screen.height && mouseY > Screen.height - panDetect) //mouse pan up
         {
-            if (transform.position.y > 3)
-                transform.position = new Vector3(transform.position.x, transform.position.y - cameraMoveSpeed, transform.position.z);
+            moveZ += cameraMoveSpeed;
         }
-        /*vertical movement */
-
-
-        /*horizontal pivot movement */
-
-        if (Input.GetKey(KeyCode.Home))
+        else if (mouseY > 0 && mouseY < panDetect) //mouse pan down
         {
-            transform.rotation = new Quaternion(transform.rotation.x, transform.rotation.y + cameraMoveSpeed/5, transform.rotation.z, transform.rotation.w);
+            moveZ -= cameraMoveSpeed;
         }
 
-        if (Input.GetKey(KeyCode.End))
-        {
-            transform.rotation = new Quaternion(transform.rotation.x, transform.rotation.y - cameraMoveSpeed/5, transform.rotation.z, transform.rotation.w);
-        }
+        Vector3 newCameraPosition = new Vector3(moveX, cameraHeight, moveZ);
 
-        /* end horizontal pivot movement */
+        Camera.main.transform.position = newCameraPosition;
     }
 }
