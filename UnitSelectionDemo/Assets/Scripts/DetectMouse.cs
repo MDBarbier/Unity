44aeebf23b16a62c97adb465d59b7d109b9698d5
matt barbier <matthewdavidbarbier@gmail.com>
2019-08-26 16:36:23 +0100

    added single unit selection demo

---
+++ UnitSelectionDemo/Assets/Scripts/DetectMouse.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectMouse : MonoBehaviour
+{    
+    private Camera mainCamera;
+    internal GameObject clickDetectedOn;
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+        mainCamera = Camera.main;        
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        RaycastHit hit;
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(ray, out hit, 100f))
+            {
+                if (hit.transform)
+                {
+                    clickDetectedOn = hit.transform.gameObject;
+
+                    if (hit.transform.tag == "ChildObject")
+                    {
+                        clickDetectedOn = hit.transform.parent.gameObject;
+                        Debug.Log($"Parent object is {hit.transform.parent.gameObject.name}");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ UnitSelectionDemo/Assets/Scripts/SelectedUnit.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectedUnit : MonoBehaviour
+{    
+    private DetectMouse detectMouse;
+    public GameObject[] units;
+    public Material selectedUnitMaterial;
+    public Material regularUnitMaterial;
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+        detectMouse = FindObjectOfType<DetectMouse>();
+        units = GameObject.FindGameObjectsWithTag("Unit");
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        //Check selected unit
+        if (detectMouse.clickDetectedOn == null)
+        {
+
+        }
+        else if ( detectMouse.clickDetectedOn.gameObject.name != "Floor")
+        {
+            //Assign the "selected" material to the selected unit
+            foreach (Transform childGO in detectMouse.clickDetectedOn.transform)
+            {
+                childGO.gameObject.GetComponent<MeshRenderer>().material = selectedUnitMaterial;
+            }
+
+            //Assign the "regular" unit material to all other Units
+            foreach (var unit in units)
+            {
+                foreach (Transform childGO in unit.transform)
+                {
+                    if (childGO.transform.parent.name == detectMouse.clickDetectedOn.name)
+                    {
+                        continue;
+                    }
+
+                    childGO.gameObject.GetComponent<MeshRenderer>().material = regularUnitMaterial;
+                }
+            }
+        }
+        else
+        {
+            //Assign the "regular" unit material to all Units
+            foreach (var unit in units)
+            {
+                foreach (Transform childGO in unit.transform)
+                {
+                    childGO.gameObject.GetComponent<MeshRenderer>().material = regularUnitMaterial;
+                }
+            }
+        }
+
+    }
+}
