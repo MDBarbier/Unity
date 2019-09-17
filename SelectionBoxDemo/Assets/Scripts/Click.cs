5d15ea28fb444ac4dc4c556e2608c826ff64f465
matt barbier <matthewdavidbarbier@gmail.com>
2019-09-17 21:05:20 +0100

    added selection box demo, multiple selection with control working

---
+++ SelectionBoxDemo/Assets/Scripts/Click.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Click : MonoBehaviour
+{
+    [SerializeField]
+    private LayerMask clickablesLayer;
+
+    private List<GameObject> selectedObjects;
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+        selectedObjects = new List<GameObject>();
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit raycastHit;
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out raycastHit, Mathf.Infinity, clickablesLayer))
+            {
+                var clickOn = raycastHit.collider.GetComponent<ClickOn>();
+
+                if (Input.GetKey("left ctrl"))
+                {
+                    if (!clickOn.currentlySelected)
+                    {
+                        selectedObjects.Add(raycastHit.collider.gameObject);
+                        clickOn.currentlySelected = true;
+                        clickOn.ClickMe();
+                    }
+                    else
+                    {
+                        selectedObjects.Remove(raycastHit.collider.gameObject);
+                        clickOn.currentlySelected = false;
+                        clickOn.ClickMe();
+                    }
+                }
+                else
+                {
+                    foreach (var item in selectedObjects)
+                    {
+                        var clickOnLocal = item.GetComponent<ClickOn>();
+                        clickOnLocal.currentlySelected = false;
+                        clickOnLocal.ClickMe();
+                    }
+
+                    selectedObjects.Clear();
+
+                    selectedObjects.Add(raycastHit.collider.gameObject);
+                    clickOn.currentlySelected = true;
+                    clickOn.ClickMe();
+                }                
+            }
+        }
+    }
+}
---
+++ SelectionBoxDemo/Assets/Scripts/ClickOn.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickOn : MonoBehaviour
+{
+    [SerializeField]
+    private Material red;
+
+    [SerializeField]
+    private Material gold;
+
+    private MeshRenderer meshRenderer;
+
+    [HideInInspector]
+    public bool currentlySelected = false;
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();    
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        
+    }
+
+    public void ClickMe()
+    {
+        if (!currentlySelected)
+        {
+            meshRenderer.material = red;
+        }
+        else
+        {
+            meshRenderer.material = gold;
+        }
+    }
+}
