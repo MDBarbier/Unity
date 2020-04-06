2443ad0f54683dcbeabc6817fa355bde1088844d
Matt Barbier <matthewdavidbarbier@gmail.com>
2020-04-06 14:45:15 +0100

    Continued work on Checkers

---
+++ Checkers/Assets/Scripts/CameraController.cs
@@ -0,0 +1,209 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float cameraMoveSpeed;
+    public float cameraMousePanSpeed;
+    public float panDetect;
+    public float rotationSpeed;
+    public float rotationAmount;
+    public float minCameraHeight;
+    public float maxCameraHeight;
+
+    private Quaternion initialRotation;
+    private float initialHeight;
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+        initialRotation = Camera.main.transform.rotation;
+        initialHeight = Camera.main.transform.position.y;
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        MoveCamera();
+        RotateCamera();
+        ResetCamera();
+    }
+
+    /// <summary>
+    /// Handles panning movement both by the keyboard and the mouse being near the edge of the screen
+    /// </summary>
+    private void MoveCamera()
+    {
+        ScreenEdgePan();
+        LateralKeyboardMovement();
+        VerticalKeyboardMovement();
+        MouseScrollWheelMovement();
+        KeyboardHorizontalRotation();
+        KeyboardVerticalRotation();
+        LateralKeyboardMovementInDirectionOfCamera();
+    }
+
+    private void LateralKeyboardMovementInDirectionOfCamera()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            Camera.main.transform.Translate(Vector3.forward * cameraMoveSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            Camera.main.transform.Translate(Vector3.left * cameraMoveSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            Camera.main.transform.Translate(Vector3.back * cameraMoveSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            Camera.main.transform.Translate(Vector3.right * cameraMoveSpeed);
+        }
+    }
+
+    private void RotateCamera()
+    {
+        Vector3 origin = Camera.main.transform.eulerAngles;
+        Vector3 destination = origin;
+
+        if (Input.GetMouseButton(2))
+        {
+            destination.x -= Input.GetAxis("Mouse Y") * (rotationAmount);
+            destination.y += Input.GetAxis("Mouse X") * (rotationAmount);
+        }
+
+        if (destination != origin)
+        {
+            Camera.main.transform.eulerAngles = Vector3.MoveTowards(origin, destination, Time.deltaTime * rotationSpeed);
+        }
+    }
+
+    private void KeyboardVerticalRotation()
+    {
+        Vector3 origin = Camera.main.transform.eulerAngles;
+        Vector3 destination = origin;
+
+        if (Input.GetKey(KeyCode.Insert))
+        {
+            destination.x -= rotationAmount;
+        }
+
+        if (Input.GetKey(KeyCode.Delete))
+        {
+            destination.x += rotationAmount;
+        }
+
+        if (destination != origin)
+        {
+            Camera.main.transform.eulerAngles = Vector3.MoveTowards(origin, destination, Time.deltaTime * rotationSpeed);
+        }
+    }
+
+    private void KeyboardHorizontalRotation()
+    {
+        Vector3 origin = Camera.main.transform.eulerAngles;
+        Vector3 destination = origin;
+
+        if (Input.GetKey(KeyCode.Home))
+        {
+            destination.y -= rotationAmount;
+        }
+
+        if (Input.GetKey(KeyCode.End))
+        {
+            destination.y += rotationAmount;
+        }
+
+        if (destination != origin)
+        {
+            Camera.main.transform.eulerAngles = Vector3.MoveTowards(origin, destination, Time.deltaTime * rotationSpeed);
+        }
+    }
+
+    private void MouseScrollWheelMovement()
+    {
+        float moveY = Camera.main.transform.position.y;
+        float scrollwheel = Input.GetAxis("Mouse ScrollWheel");
+        moveY -= scrollwheel * (cameraMousePanSpeed * 20);
+        moveY = Mathf.Clamp(moveY, minCameraHeight, maxCameraHeight);
+        Vector3 newPos = new Vector3(Camera.main.transform.position.x, moveY, Camera.main.transform.position.z);
+        Camera.main.transform.position = newPos;
+    }
+
+    private void VerticalKeyboardMovement()
+    {
+        if ((Input.GetKey(KeyCode.PageUp)) && transform.position.y < maxCameraHeight)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + cameraMoveSpeed, transform.position.z);
+        }
+
+        if (Input.GetKey(KeyCode.PageDown) && transform.position.y > minCameraHeight)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y - cameraMoveSpeed, transform.position.z);
+        }
+    }
+
+    private void LateralKeyboardMovement()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position = new Vector3(transform.position.x + cameraMoveSpeed, transform.position.y, transform.position.z);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position = new Vector3(transform.position.x - cameraMoveSpeed, transform.position.y, transform.position.z);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - cameraMoveSpeed);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + cameraMoveSpeed);
+        }
+    }
+
+    private void ScreenEdgePan()
+    {
+        float moveX = Camera.main.transform.position.x;
+        float moveZ = Camera.main.transform.position.z;
+        float mouseX = Input.mousePosition.x;
+        float mouseY = Input.mousePosition.y;
+        float cameraHeight = transform.position.y;
+
+        if (mouseX > 0 && mouseX < panDetect) //mouse pan left
+        {
+            moveX -= cameraMoveSpeed;
+        }
+        else if (mouseX < Screen.width && mouseX > Screen.width - panDetect) //mouse pan right
+        {
+            moveX += cameraMoveSpeed;
+        }
+
+        if (mouseY < Screen.height && mouseY > Screen.height - panDetect) //mouse pan up
+        {
+            moveZ += cameraMoveSpeed;
+        }
+        else if (mouseY > 0 && mouseY < panDetect) //mouse pan down
+        {
+            moveZ -= cameraMoveSpeed;
+        }
+
+        Vector3 newCameraPosition = new Vector3(moveX, cameraHeight, moveZ);
+
+        Camera.main.transform.position = newCameraPosition;
+    }
+
+    private void ResetCamera()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            Camera.main.transform.rotation = initialRotation;
+            Camera.main.transform.position = new Vector3(Camera.main.transform.position.x, initialHeight, Camera.main.transform.position.z);
+        }
+    }
+}
---
+++ Checkers/Assets/Scripts/DetectMouse.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class DetectMouse : MonoBehaviour
+{
+    private Camera mainCamera;
+    internal GameObject clickDetectedOn;
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        RaycastHit hit;
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(ray, out hit, 100f))
+            {
+                if (hit.transform)
+                {
+                    if (hit.transform.tag == "WhitePieces" || hit.transform.tag == "BlackPieces")
+                    {
+                        clickDetectedOn = hit.transform.parent.gameObject;
+                        var parent = hit.transform.parent.gameObject;
+                        var colour = hit.transform.tag == "WhitePieces" ? "White" : "Black";
+                        Debug.Log($"The piece clicked was {colour}, and is in {parent.name}, {parent.transform.parent.name}");
+                    }
+
+                    clickDetectedOn = hit.transform.gameObject;
+                }
+            }
+        }
+    }
+}
