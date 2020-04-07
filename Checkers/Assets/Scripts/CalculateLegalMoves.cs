65b478ccc27d2ae0b4c13228b90e53c9d72f3249
Matt Barbier <matthewdavidbarbier@gmail.com>
2020-04-07 11:45:21 +0100

    Checkers: Work on calculating legal moves

---
+++ Checkers/Assets/Scripts/CalculateLegalMoves.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CalculateLegalMoves : MonoBehaviour
+{
+    public GameObject[] legalMoves;
+
+    public GameObject[] GetLegalMoves(GameObject piece)
+    {
+        var legalSquareList = new List<GameObject>();
+        var legalMoveList = new Dictionary<GameObject, List<GameObject>>();
+
+        //Calculate details about the piece and where it is located
+        var squareName = piece.transform.parent.gameObject.name;
+        var columnName = piece.transform.parent.gameObject.transform.parent.name;
+        var colour = piece.transform.tag == "WhitePieces" ? "White" : "Black";
+        var squareNumber = int.Parse(squareName.Split('_')[1]);
+        var columnNumber = int.Parse(columnName.Split('_')[1]);
+
+        //Calculate all single square moves
+        var leftColumn = GameObject.Find($"Column_{columnNumber-1}");
+        var rightColumn = GameObject.Find($"Column_{columnNumber + 1}");
+
+        if (leftColumn != null)
+        {            
+            var childCount = leftColumn.gameObject.transform.childCount;
+            GameObject[] squares = new GameObject[childCount];
+
+            for (int i = 0; i < childCount; i++)
+            {
+                squares[i] = leftColumn.gameObject.transform.GetChild(i).gameObject;
+            }
+
+            if (squareNumber + 1 > 7 || squareNumber + 1 < 0)
+            {
+                //out of bounds
+            }
+            else
+            {
+                var upperMove = squares[squareNumber + 1];
+                legalSquareList.Add(upperMove);
+            }
+
+            if (squareNumber -1 >7 || squareNumber -1 <0)
+            {
+                // out of bounds
+            }
+            else
+            {
+                var lowerMove = squares[squareNumber - 1];
+                legalSquareList.Add(lowerMove);
+            }
+
+            legalMoveList.Add(leftColumn, legalSquareList);
+        }
+
+        legalSquareList.RemoveAll(a => a);
+
+        if (rightColumn != null)
+        {
+            var childCount = rightColumn.gameObject.transform.childCount;
+            GameObject[] squares = new GameObject[childCount];
+
+            for (int i = 0; i < childCount; i++)
+            {
+                squares[i] = rightColumn.gameObject.transform.GetChild(i).gameObject;
+            }
+
+            if (squareNumber + 1 > 7 || squareNumber + 1 < 0)
+            {
+                //out of bounds
+            }
+            else
+            {
+                var upperMove = squares[squareNumber + 1];
+                legalSquareList.Add(upperMove);
+            }
+
+            if (squareNumber - 1 > 7 || squareNumber - 1 < 0)
+            {
+                // out of bounds
+            }
+            else
+            {
+                var lowerMove = squares[squareNumber - 1];
+                legalSquareList.Add(lowerMove);
+            }
+
+            legalMoveList.Add(rightColumn, legalSquareList);
+        }
+
+        string output = string.Empty;
+
+        foreach (var move in legalMoveList)
+        {           
+
+            foreach (var square in move.Value)
+            {                
+                output += $"{square.transform.gameObject.name}, {move.Key.gameObject.name} |";
+            }
+        }
+
+        output = output.TrimEnd('|');
+
+        Debug.Log("The detected legal moves are: " + output);
+
+        return new GameObject[] { };
+    }
+}
--- Checkers/Assets/Scripts/SelectPiece.cs
+++ Checkers/Assets/Scripts/SelectPiece.cs
@@ -5,16 +5,22 @@ using UnityEngine;
 public class SelectPiece : MonoBehaviour
 {
     private DetectMouse detectMouse;
+    private SceneManager sceneManager;
+    private CalculateLegalMoves calculateLegalMoves;
     public GameObject[] pieces;
     public Material whitePieceMaterial;
     public Material blackPieceMaterial;
     public Material selectedWhitePieceMaterial;
     public Material selectedBlackPieceMaterial;
+    private GameObject[] legalMoves;
+    private GameObject selectedPiece;
 
     // Start is called before the first frame update
     public void Start()
     {
         detectMouse = FindObjectOfType<DetectMouse>();
+        sceneManager = FindObjectOfType<SceneManager>();
+        calculateLegalMoves = FindObjectOfType<CalculateLegalMoves>();
 
         //Get all the white and black pieces and add them to the pieces array
         var whitePieces = GameObject.FindGameObjectsWithTag("WhitePieces");
@@ -33,10 +39,17 @@ public class SelectPiece : MonoBehaviour
             return;
         }        
 
-        if (!detectMouse.clickDetectedOn.gameObject.name.Contains("Square") && detectMouse.clickDetectedOn.gameObject.name != "Board")
+        if (!detectMouse.clickDetectedOn.gameObject.name.Contains("Square") && 
+            detectMouse.clickDetectedOn.gameObject.name != "Board" &&
+            detectMouse.clickDetectedOn.gameObject.tag.Contains(sceneManager.PlayerColour.ToString()))
         {
             //Assign the appropriate "selected" material to the selected piece
-            detectMouse.clickDetectedOn.gameObject.GetComponent<MeshRenderer>().material = GetCorrectMaterial(detectMouse.clickDetectedOn.gameObject, true);
+            detectMouse.clickDetectedOn.gameObject.GetComponent<MeshRenderer>().material = GetCorrectMaterial(detectMouse.clickDetectedOn.gameObject, true);            
+
+            if (selectedPiece != detectMouse.clickDetectedOn.gameObject || legalMoves == null)
+            {
+                legalMoves = calculateLegalMoves.GetLegalMoves(detectMouse.clickDetectedOn.gameObject);
+            }
 
             //Assign the "regular" unit material to all other pieces
             foreach (var unit in pieces)
@@ -49,6 +62,9 @@ public class SelectPiece : MonoBehaviour
                 unit.gameObject.GetComponent<MeshRenderer>().material = GetCorrectMaterial(unit.gameObject, false);
 
             }
+
+            //Set the selected piece for comparison on next update
+            selectedPiece = detectMouse.clickDetectedOn.gameObject;
         }
         else
         {
